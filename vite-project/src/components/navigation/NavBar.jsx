dda95ddb5e1debadacd1b59cb71a40c3bfa5ffa8
Bob Deckard <bdeckard92@gmail.com>
2026-02-09 16:12:14 -0600

    updates navbar to spread links

--- vite-project/src/components/navigation/NavBar.jsx
+++ vite-project/src/components/navigation/NavBar.jsx
@@ -1,14 +1,17 @@
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
 import './NavBar.css'
 
 const NavBar = () => {
-    return (
-         <nav>
-                <Link to="/">Home</Link> |{" "}
-                <Link to="/videos">Video Library</Link> |{" "}
-                <Link to="/about">About Me</Link> |{" "}
-                <Link to="/blog">Blog Posts</Link>
-              </nav>
-    )
+  return (
+    <nav>
+      <div className="nav-inner">
+        <Link to="/">Home</Link>
+        <Link to="/videos">Video Library</Link>
+        <Link to="/about">About Me</Link>
+        <Link to="/blog">Blog Posts</Link>
+      </div>
+    </nav>
+  )
 }
+
 export default NavBar;
